d2d9a1261519155b84423ab3656b65c03d236623
Mushfiqur0R <musfikur.dev@gmail.com>
2024-03-21 16:54:55 +0600

    react,tailwind, daisyUI setup with favicon

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>      
+      <h1 className='text-3xl'>Vite + React</h1>
+      <button class="btn btn-outline btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
